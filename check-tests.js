2b0271493f33dcced1ba1a83abab9469f496b49d
Xingchen Hong <hello@xc-h.net>
2016-01-12 13:25:55 -0600

    Add test cases.

--- check-tests.js
+++ check-tests.js
@@ -1,5 +1,20 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add('Backward compatibility', function (test) {
+  test.throws(function () {
+    check('foo', Number);
+  });
+});
+
+Tinytest.add('Can throw error', function (test) {
+  test.throws(function () {
+    check('foo', Number, 'Custom message');
+  });
+});
+
+Tinytest.add('Custom message', function (test) {
+  let errorMsg = 'Expect a number.';
+  try {
+    check('foo', Number, errorMsg);
+  } catch (error) {
+    test.equal(errorMsg, error.message);
+  }
 });
