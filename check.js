f841e4d0d707bb107c251115333c82c47f865492
Xingchen Hong <hello@xc-h.net>
2016-01-14 15:17:07 -0600

    Improve performance.

--- check.js
+++ check.js
@@ -1,13 +1,18 @@
 // Keep a reference to the old check.
 let _check = check;
+// According to https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#2-unsupported-syntax,
+// refacor try-catch block to make the containing function optimizable.
+let nonoptimizableTryCatchBlock = function (value, pattern, message) {
+  try {
+    _check(value, pattern);
+  } catch (error) {
+    throw new Error(message);
+  }
+};
 check = function (value, pattern, message) {
   if (typeof message === 'undefined') {
     _check(value, pattern);
   } else {
-    try {
-      _check(value, pattern);
-    } catch (error) {
-      throw new Error(message);
-    }
+    nonoptimizableTryCatchBlock(value, pattern, message);
   }
 };
